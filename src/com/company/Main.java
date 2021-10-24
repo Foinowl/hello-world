8e0235de960e0e671b7c13c2fe148d2f2b324054
Evgeniy <nifpopnaf@gmail.com>
2021-10-24 21:13:57 +0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world);
+    }
+}
